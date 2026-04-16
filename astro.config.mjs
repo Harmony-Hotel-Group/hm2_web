294b525dcb463b773cc12223b2b9b50e4f0af6da
WalterCun <waltercunbustamante@gmail.com>
2026-04-16 18:36:21 -0500

    feat: add TypeScript configuration file with path aliases for components and utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@organisms': '/src/components/organisms',
+        '@molecules': '/src/components/molecules',
+        '@atoms': '/src/components/atoms',
+        '@templates': '/src/components/templates',
+        '@lib': '/src/lib',
+        '@utils': '/src/utils',
+        '@styles': '/src/styles',
+        '@layouts': '/src/layouts',
+        '@pages': '/src/pages',
+        '@data': '/src/data'
+      }
+    }
+  }
 });
